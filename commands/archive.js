dd6fe55c6ce965505d7b1ab1e3e5b14bbfccb07e
Pavel Nezhencev <whyleee@gmail.com>
2015-08-15 00:45:46 +0300

    Remove archive verbose output

--- commands/archive.js
+++ commands/archive.js
@@ -1,12 +1,7 @@
 var jsgit = require('../lib/js-git-api');
 
 module.exports = function archive(ref, options) {
-  var repo = jsgit.repo(process.cwd());
-  
-  if (options.verbose) {
-    options.onProgress = console.log;
-  }
-  
+  var repo = jsgit.repo(process.cwd());  
   jsgit.archive(repo, ref, options, function(err) {
     if (err) throw err;
   });
