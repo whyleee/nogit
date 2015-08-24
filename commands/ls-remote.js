d2961f8221a87cb55f7d6197d253e6dfb8b7f5ba
Pavel Nezhencev <whyleee@gmail.com>
2015-08-24 12:06:50 +0300

    Fix ls-remote when both tags and heads required

--- commands/ls-remote.js
+++ commands/ls-remote.js
@@ -1,17 +1,21 @@
 var jsgit = require('../lib/js-git-api');
 
 function lsRemote(repoUrl, options) {
+  var getAll = !options.heads && !options.tags;
   var remote = jsgit.remote(repoUrl);
   jsgit.ls(remote, function(err, refs) {
     if (err) throw err;
     Object.keys(refs).forEach(function (ref) {
-      if (options.tags && !~ref.indexOf('refs/tags/')) {
-        return;
+      var wanted = false;
+      if (options.heads && ~ref.indexOf('refs/heads/')) {
+        wanted = true;
       }
-      if (options.heads && !~ref.indexOf('refs/heads/')) {
-        return;
+      if (options.tags && ~ref.indexOf('refs/tags/')) {
+        wanted = true;
       }
-      console.log(refs[ref] + "\t" + ref);
+      if (getAll || wanted) {
+        console.log(refs[ref] + "\t" + ref);
+      }
     });
   });
 }
