ea27bb6ec981192ec3c246a88f2078c1758f2139
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2015-09-21 22:56:05 +0200

    Save packed-refs and use them plus log walkers to find a hash for any ref in rev-list

--- commands/rev-list.js
+++ commands/rev-list.js
@@ -1,32 +1,12 @@
 var jsgit = require('../lib/js-git-api');
 
-module.exports = function(branch, options) {
-  // only '-n1' and 'refs/heads' are supported
+module.exports = function(ref, options) {
+  // only '-n1' are supported
   if (options.maxCount == 1) {
     var repo = jsgit.repo();
-    repo.readRef('refs/heads/' + branch, function(err, hash) {
+    jsgit.readRef(repo, ref, function(err, hash) {
       if (err) throw err;
-      if (hash) {
-        console.log(hash);
-      } else {
-        readRemoteRef(branch, function(err, hash) {
-          if (err) throw err;
-          console.log(hash);
-        });
-      }
+      console.log(hash);
     });
   }
 };
-
-function readRemoteRef(branch, callback) {
-  var remote = jsgit.remote(jsgit.getRemoteUrl());
-  jsgit.ls(remote, function(err, refs) {
-    if (err) return callback(err);
-    
-    var ref = Object.keys(refs).filter(function(ref) {
-      return ref.endsWith('/' + branch); 
-    })[0];
-    
-    return callback(null, refs[ref]);
-  });
-}
